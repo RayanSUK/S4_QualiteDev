0f905dae8c5999afda480908703a4cb82b71bf44
RayanSUK <sukkarie.rayan@gmail.com>
2025-02-01 15:07:03 +0100

    Ajout du dossier doc, ajout Javadoc pour la classe ArmementSupplementaire

--- src/main/java/ArmementSupplementaire.java
+++ src/main/java/ArmementSupplementaire.java
@@ -1,14 +1,36 @@
+/**
+ * La classe abstraite ArmementSupplementaire représente un décorateur pour les voiliers.
+ * Elle permet d'ajouter des fonctionnalités supplémentaires à un objet Voilier existant.
+ */
 public abstract class  ArmementSupplementaire extends Voilier   {
+
+    /** Le voilier décoré par cet armement supplémentaire. */
     Voilier composantadecore;
 
+
+    /**
+     * Constructeur de la classe ArmementSupplementaire.
+     *
+     * @param composantadecore Le voilier à décorer avec un armement supplémentaire.
+     */
     public ArmementSupplementaire(Voilier composantadecore) {
         super();
         this.composantadecore = composantadecore;
 
     }
+
+    /**
+     * La méthode suivre route permet d'indiquer au voilier décorer quelle route suivre
+     */
     public void appliqueSuivreRoute(){
         composantadecore.appliquerSuivreRoute();
     }
+
+    /**
+     * Définit une nouvelle stratégie de suivi de route pour le voilier décoré.
+     *
+     * @param suivreRoute La stratégie de suivi de route à appliquer.
+     */
     public void setSuivreRoute(SuivreRoute suivreRoute) {
         composantadecore.setSuivreRoute(suivreRoute);
     }
